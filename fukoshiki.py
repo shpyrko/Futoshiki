3b0b84c2455dba736f7c39a18b56fd64f551de9b
Alex Shpyrko <alex.shpyrko@gmail.com>
2020-05-12 20:19:15 -0400

    finished implementation

--- fukoshiki.py
+++ fukoshiki.py
@@ -3,7 +3,7 @@
 class Fukoshiki():
 
     def __init__(self, input_file):
-        self.num_cells = 4
+        self.num_cells = 5
         self.matrix = []
         self.row_constraints = []
         self.col_constraints = []
@@ -12,42 +12,24 @@ class Fukoshiki():
 
     def get_matrix(self, filename):
         with open(filename, 'r') as file:
-            for i in range(self.num_cells+1):
+            for i in range(self.num_cells):
                 str_row = file.readline().strip().split()
                 int_row = [int(num) for num in str_row]
                 self.matrix.append(int_row)
             file.readline()
 
-            for i in range(self.num_cells+1):
+            for i in range(self.num_cells):
                 row = file.readline().strip().split()
                 self.row_constraints.append(row)
             file.readline()
 
-            for i in range(self.num_cells):
+            for i in range(self.num_cells-1):
                 row = file.readline().strip().split()
                 self.col_constraints.append(row)
         print(self.matrix)
         print(self.row_constraints)
         print(self.col_constraints)
 
-
-    def get_row_constraints(self, filename):
-        row_const = []
-        with open(filename, 'r') as file:
-            for i in range(self.num_cells):
-                row = file.readline().strip().split()
-                row_const.append(row)
-        print(row_const)
-        return row_const
-
-    def get_col_constraints(self, filename):
-        col_const = []
-        with open(filename, 'r') as file:
-            for i in range(self.num_cells):
-                row = file.readline().strip().split()
-                col_const.append(row)
-        print(col_const)
-        return col_const
 
     def satisfies_constraints(self, val, row_num, col_num):
         if col_num > 0:
@@ -115,7 +97,7 @@ class Fukoshiki():
         if not cell:
             return True
 
-        for num in range(1, self.num_cells + 2):
+        for num in range(1, self.num_cells + 1):
             if (self.allowed_in_row(num, cell[0]) and
                     self.allowed_in_col(num, cell[1]) and
                     self.satisfies_constraints(num, cell[0], cell[1])):
@@ -128,4 +110,4 @@ class Fukoshiki():
         return False
 
 
-Fukoshiki("Input1.txt").solve(verbose=True)
+Fukoshiki("my_input").solve(verbose=True)
